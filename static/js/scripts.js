308884a0d7e3ab2a5ccf83f0340094ce4ace2b5d
Brad Sawicki <git@bradsawicki.com>
2014-01-09 12:04:58 -0600

    Init Static Build - Home

---
+++ static/js/scripts.js
@@ -0,0 +1,24 @@
+$(function(){
+	
+	$(window).on('resize', function() {
+	    resize_thumbs();
+	});
+	resize_thumbs();
+	
+	// Mobile Button
+	$('.cat-open').click(function(){
+		$(this).hide();
+		$('.cat-nav').show();
+		return false;
+	});
+	
+	$('.show a').click(function(){
+    	$('.main-nav').removeClass('show');
+	});
+
+    function resize_thumbs() {
+        var y = $('.thumb').width();
+        $('.project-title').css('height', y-4).css('width', y-4);
+    }
+
+});
